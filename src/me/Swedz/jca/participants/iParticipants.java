80a87394a37f9d1fe7adac5e62136e45d74bfec2
Swedz <email@jonahdunham.com>
2016-11-06 17:44:31 -0500

    Create iParticipants.java

---
+++ src/me/Swedz/jca/participants/iParticipants.java
@@ -0,0 +1,6 @@
+package me.Swedz.jca.participants;
+
+public interface iParticipants {
+	public int seed(String participant) throws Exception;
+	public int id(String participant) throws Exception;
+}
