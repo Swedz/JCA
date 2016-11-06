f8a500da68153e1d38bcae811a1a8dee7d859ce5
Swedz <email@jonahdunham.com>
2016-11-06 17:43:29 -0500

    Create iChallonge.java

---
+++ src/me/Swedz/jca/challonge/iChallonge.java
@@ -0,0 +1,7 @@
+package me.Swedz.jca.challonge;
+
+public interface iChallonge {
+	public String getParticipants() throws Exception;
+	public String getMatches() throws Exception;
+	public void start() throws Exception;
+}
